3b7f4edeb86cb8fe95cee52853223ed66c684dff
MohammedBasha <me@mohammedbasha.com>
2021-07-31 23:12:47 +0200

    Adding the validation class methods for emails and string lenght

--- includes/validationClass.php
+++ includes/validationClass.php
@@ -2,5 +2,18 @@
 
 class validationClass
 {
+    public static function validateMail($email) {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
 
+    public static function validateLength($str, $length, $compare = '<') {
+        if ($compare == '<') {
+            return strlen($str) < $length;
+            return false;
+        } elseif ($compare == '>') {
+            return strlen($str) > $length;
+            return false;
+        }
+        return false;
+    }
 }
