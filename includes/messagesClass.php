ea09cccab6e16560662d1344f80bd21f596821bb
MohammedBasha <me@mohammedbasha.com>
2021-07-31 22:54:57 +0200

    Finishing the messages class with all of its database connection and methods and had been tested

--- includes/messagesClass.php
+++ includes/messagesClass.php
@@ -2,17 +2,94 @@
 
 class messagesClass
 {
-    public function addMessage() {}
+    private $connection;
+    private $server = SERVER;
+    private $dbname = DBNAME;
+    private $options = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ];
 
-    public function getMessages() {}
+    public function __construct() {
+        try {
+            $this->connection = new PDO(
+                "mysql:host=$this->server;dbname=$this->dbname",
+                DBUSER,
+                DBPASS,
+                $this->options
+            );
+        } catch (PDOException $e) {
+            echo 'Error connecting the database, ' . $e->getMessage();
+        }
+    }
 
-    public function getMessage() {}
+    public function addMessage(message $m) {
+        $title      = $m->getTitle();
+        $message    = $m->getMessage();
+        $name       = $m->getName();
+        $email      = $m->getEmail();
+        $phone      = $m->getPhone();
+        $published  = $m->getPublished();
 
-    public function searchMessages() {}
+        try {
+            $query = $this->connection->prepare("INSERT INTO messages(title, message, name, email, phone, published, date) VALUES (?, ?, ?, ?, ?, ?, NOW())");
 
-    public function getMessagesByStatus() {}
+            $query->execute([$title, $message, $name, $email, $phone, $published]);
+            return true;
+        } catch (PDOException $e) {
+            echo 'Error Inserting data, ' . $e->getMessage();
+        }
+    }
 
-    public function updateMessage() {}
+    public function getMessages($extras = null) {
+        try {
+            $query = $this->connection->prepare("SELECT * FROM messages $extras");
 
-    public function deleteMessage() {}
+            $query->execute();
+            return $query->fetchAll();
+
+        } catch (PDOException $e) {
+            echo 'Error fetching data, ' . $e->getMessage();
+        }
+    }
+
+    public function getMessage($id) {
+        $query = $this->getMessages("WHERE id = $id");
+        return count($query) > 0? $query[0] : [];
+    }
+
+    public function searchMessages($keyword) {
+        return $this->getMessages("WHERE title LIKE '%$keyword%' OR message LIKE '%$keyword%'");
+    }
+
+    public function getMessagesByStatus($published = 1) {
+        return $this->getMessages("WHERE published = $published");
+    }
+
+    public function updateMessage(message $m) {
+        $id         = $m->getId();
+        $title      = $m->getTitle();
+        $message    = $m->getMessage();
+        $published  = $m->getPublished();
+
+        try {
+            $query = $this->connection->prepare("UPDATE messages SET title = ?, message = ?, published = ? WHERE id = ?");
+
+            $query->execute([$title, $message, $published, $id]);
+            return true;
+        } catch (PDOException $e) {
+            echo 'Error Updating data, ' . $e->getMessage();
+        }
+    }
+
+    public function deleteMessage($id) {
+        try {
+            $query = $this->connection->prepare("DELETE FROM messages WHERE id = ?");
+
+            $query->execute([$id]);
+            return true;
+        } catch (PDOException $e) {
+            echo 'Error Deleting data, ' . $e->getMessage();
+        }
+    }
 }
