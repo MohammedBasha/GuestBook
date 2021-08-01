8f88389d3aaeb0746227ff3717e4588b5df0838d
MohammedBasha <me@mohammedbasha.com>
2021-08-01 12:42:38 +0200

    Adding the add message page

--- add.php
+++ add.php
@@ -1,2 +1,17 @@
 <?php
 
+include 'includes/config.php';
+include 'includes/message.php';
+include 'includes/messagesClass.php';
+
+if (count($_POST) > 0) {
+    $message = new message();
+    $message->setTitle($_POST['title']);
+    $message->setMessage($_POST['message']);
+    $message->setName($_POST['name']);
+    $message->setEmail($_POST['email']);
+    $message->setPhone($_POST['phone']);
+
+    $messageAdd = new messagesClass();
+    $messageAdd->addMessage($message);
+}
--- index.php
+++ index.php
@@ -3,4 +3,9 @@
 include 'includes/config.php';
 include 'includes/message.php';
 include 'includes/messagesClass.php';
+
+$messages = new message();
+$messagesClass = new messagesClass();
+$allMessages = $messagesClass->getMessages('WHERE published = 1 ORDER BY id DESC');
+
 include 'templates/frontend/index.php';
